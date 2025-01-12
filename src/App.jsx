2edb77163d37aad77a8347ff7ff8e3f17a52c820
samdev <banyatsisam@gmail.com>
2025-01-13 01:14:59 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,10 @@ function App() {
 				<h2 className="text-2xl md:text-4xl lg:text-6xl uppercase">Welcome to</h2>
 				<h1 className="text-3xl md:text-6xl lg:text-8xl uppercase font-black mb-8">The coding club</h1>
 
-				<p className="text-base md:text-lg lg:text-2xl mb-8">A place for developers to code in peace without those pesky project managers!</p>
+				<p className="text-base md:text-lg lg:text-2xl mb-8">A place where farmers meet!</p>
 
 				<div className="text-lg md:text-2xl lg:text-3xl py-2 px-4 md:py-4 md:px-10 lg:py-6 lg:px-12 bg-white bg-opacity-10 w-fit mx-auto mb-8 rounded-full">
-					943,245 members
+					345 members
 				</div>
 
 				<form 
